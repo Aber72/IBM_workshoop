b22e1ffd96160b9e65cfe39f541d3d3f4b2b4119
abhishek <abh9204@gmail.com>
2025-05-01 21:26:22 +0530

    learn about string  immuatibility

--- eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
+++ eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
@@ -7,21 +7,21 @@ public class Application
 		
 		
 		
-		 StringBuilder sb = new StringBuilder("Welcome to IBM");
-	        System.out.println(sb); // prints "Welcome to IBM"
+		 String s = "Welcome to IBM";
+	        System.out.println(s); // prints "Welcome to IBM"
 
-	        // modify the StringBuilder
-	        sb.append("!");
-	        System.out.println(sb); // prints "Welcome to IBM!"
+	        // try to modify the String
+	        s = s + "!";
+	        System.out.println(s); // prints "Welcome to IBM!"
 
-	        // assign the StringBuilder to a new variable
-	        StringBuilder sb2 = sb;
-	        System.out.println(sb2); // prints "Welcome to IBM!"
+	        // assign the String to a new variable
+	        String s2 = s;
+	        System.out.println(s2); // prints "Welcome to IBM!"
 
-	        // modify the StringBuilder through the new variable
-	        sb2.append(" We are excited to have you!");
-	        System.out.println(sb); // prints "Welcome to IBM! We are excited to have you!"
-	        System.out.println(sb2); // prints "Welcome to IBM! We are excited to have you!"
+	        // try to modify the String through the new variable
+	        s2 = s2 + " We are excited to have you!";
+	        System.out.println(s); // still prints "Welcome to IBM!"
+	        System.out.println(s2); // prints "Welcome to IBM! We are excited to have you!"
 		    
 		
 
