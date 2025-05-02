4cc25a27a6382ff50963822439dbe383253c79e8
abhishek <abh9204@gmail.com>
2025-05-02 10:32:45 +0530

    doen exception handeling

--- eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
+++ eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
@@ -5,20 +5,16 @@ public class Application
 	public static void main( String[] args )
 	{
 		
+		System.out.println("start");
 		
-		// Declare and initialize a 2D array of integers
-        int[][] numbers = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-        };
-
-        // Print the 2D array using an enhanced for loop
-        for (int[] row : numbers) {
-            for (int number : row) {
-                System.out.print(number + " ");
-            }
-            System.out.println();}
+		try {
+			System.out.println(args[0]);
+		}
+		catch(Exception ex){
+			System.out.println("enter correct input");
+			
+		}
+	
 
 		    
 		
