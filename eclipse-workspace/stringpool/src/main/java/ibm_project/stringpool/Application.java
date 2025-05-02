f5765c66fa8a15d017269a64a118fee6b18f5747
abhishek <abh9204@gmail.com>
2025-05-02 11:24:06 +0530

    default exception handling ,nested try catch and finally block execution

--- eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
+++ eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
@@ -7,11 +7,36 @@ public class Application
 		
 		System.out.println("start");
 		
+		
+		
 		try {
-			System.out.println(args[0]);
+			String st = args[0];
+			String st1 = args[1];
+			try {
+				System.out.println(Integer.parseInt(st)/Integer.parseInt(st1));
+			}
+			
+			catch(NumberFormatException ex) {
+				System.out.println("nested catch");
+				
+			}
+			finally {
+				System.out.println("its finally block");
+			}
+			System.out.println("end of inner try");
+			
 		}
+		catch(ArrayIndexOutOfBoundsException  |NumberFormatException ex){
+			System.out.println("make sure u r providing correct numbers");
+			
+		}
+		catch(ArithmeticException ex) {
+			System.out.println("number cannot divide by zero");
+		}
+		
 		catch(Exception ex){
-			System.out.println("enter correct input");
+			System.out.println("unknown error");
+			ex.printStackTrace();
 			
 		}
 	
