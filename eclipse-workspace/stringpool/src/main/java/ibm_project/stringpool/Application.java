951747d06f0b63d6898f7683c240b23d5cc55047
abhishek <abh9204@gmail.com>
2025-05-02 12:34:17 +0530

    fixed how to add assert method to handle logical error

--- eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
+++ eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
@@ -1,35 +1,21 @@
 package ibm_project.stringpool;
-class IllegalAgeException extends Exception{
-	@Override
-	public String getMessage() {
-		// TODO Auto-generated method stub
-		return "your age is not 18";
-	}
-	}
+
 	
-class ElectionVoting{
-	public void register(int age)throws IllegalAgeException{
-		if(age<18) {
-			throw new IllegalAgeException();
-		}
-	}
-}
+import math.*;
 
 public class Application
 {
 	public static void main( String[] args )
+	
+	
 	{
-		ElectionVoting electionVoting = new ElectionVoting();
 		
-		try {
-			electionVoting.register(17);
-			
-		}
-		catch(IllegalAgeException ex) {
-			ex.printStackTrace();
-			
-		}
-		System.out.println("end");
+			System.out.println("start");
+			Arithamtic arithamtic = new Arithamtic();
+			assert(arithamtic.sum(4,3)==7):"this is sum method";
+			System.out.println("end");
+		
+		
 	
 		    
 		
---
+++ eclipse-workspace/stringpool/src/main/java/math/Arithamtic.java
@@ -0,0 +1,9 @@
+package math;
+
+public class Arithamtic {
+	
+	public int sum(int i1,int i2) {
+		int sum = i1*i2;
+	}
+
+}
