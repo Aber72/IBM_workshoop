ec5b308f207a9a1b4c8fea11cb184387d1fc4a23
abhishek <abh9204@gmail.com>
2025-05-03 07:48:52 +0530

    Type Casting

--- eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
+++ eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
@@ -1,35 +1,69 @@
 package ibm_project.stringpool;
 
 	
-class Runner implements Runnable{
-	public void run() {
-		for(int i=0;i<10;i++) {
-			System.out.println(Thread.currentThread()+-->+String.valueOf(c));
-		}
-	}
-}
+
 
 public class Application
 {
-	public static void main( String[] args )
+	public static void main( String[] args ) {
 	
 	
-	{
 		
-			Runner runner = new Runner();
-			Thread thread1 = new Thread();
-			System.out.println(thread1.getState());
-			thread1.start();
-			System.out.println(thread1.getState());
-			try {
-				Thread.sleep(100);
-			}catch(InterruptedException e) {
-				e.printStackTrace();
-			}
-		System.out.println(thread1.getState());
-	
-		    
-		
+        int intValue = 42;
+        double doubleValue = intValue;
+        System.out.println("Widening Casting:");
+        System.out.println("int value: " + intValue);
+        System.out.println("double value: " + doubleValue);
 
-	}
+        
+        double originalDouble = 42.99;
+        int narrowedInt = (int) originalDouble; 
+        System.out.println("\nNarrowing Casting:");
+        System.out.println("Original double value: " + originalDouble);
+        System.out.println("Narrowed int value: " + narrowedInt);
+
+       
+        System.out.println("\n=== Reference Typecasting ===");
+
+      
+        Animal animal = new Dog(); 
+        animal.makeSound();
+
+      
+        if (animal instanceof Dog) { 
+            Dog dog = (Dog) animal; 
+            dog.bark(); 
+        }
+
+       
+        System.out.println("\nInvalid Downcasting Example:");
+        Animal genericAnimal = new Animal();
+        if (genericAnimal instanceof Dog) {
+            Dog wrongDog = (Dog) genericAnimal; 
+            wrongDog.bark();
+        } else {
+            System.out.println("Downcasting failed: genericAnimal is not a Dog.");
+        }
+    }
+}
+
+
+class Animal {
+    public void makeSound() {
+        System.out.println("The animal makes a sound.");
+    }
+}
+
+
+class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("The dog says: Woof!");
+    }
+
+    public void bark() {
+        System.out.println("The dog barks!");
+    }
+
+	
 }
