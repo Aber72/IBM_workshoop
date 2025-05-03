fe6a1b4fd4b51bf279ae0f5e6f01e0f58a3ebb6c
abhishek <abh9204@gmail.com>
2025-05-03 09:24:03 +0530

    explicitly loading class to memory

--- eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
+++ eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
@@ -3,46 +3,26 @@ package ibm_project.stringpool;
 
 public class Application {
   
-    static int staticCounter;
-
     
-    int instanceCounter;
-
    
-    static {
-        System.out.println("Static block executed.");
-        staticCounter = 100; 
-    }
-
-    {
-        System.out.println("Instance initializer block executed.");
-        instanceCounter = 10; 
-    }
-
-   
-    public Application() {
-        System.out.println("Constructor executed.");
-        instanceCounter += 5;
-    }
+    
 
     public static void main(String[] args) {
-        System.out.println("Main method executed.");
+    	
+    	 try {
+             
+             ClassLoader customClassLoader = ClassLoader.getSystemClassLoader();
+
+             
+             Class<?> loadedClass = customClassLoader.loadClass("ibm_project.stringpool.Application");
+             
+            
+             System.out.println("Class loaded explicitly using custom ClassLoader: " + loadedClass.getName());
+         } catch (ClassNotFoundException e) {
+             System.out.println("Class not found: " + e.getMessage());
+         }
+
         
        
-        System.out.println("Static Counter: " + staticCounter);
-
-       
-        System.out.println("\nCreating first object:");
-        Application app1 = new Application();
-        System.out.println("Instance Counter (app1): " + app1.instanceCounter);
-
-      
-        System.out.println("\nCreating second object:");
-        Application app2 = new Application();
-        System.out.println("Instance Counter (app2): " + app2.instanceCounter);
-
-        
-        staticCounter += 50;
-        System.out.println("\nModified Static Counter: " + staticCounter);
     }
 }
