d16d4458681d9331a16bb299560e0074d9f50225
abhishek <abh9204@gmail.com>
2025-05-05 10:33:43 +0530

    how to use this key word & comapre 2 object by overriding equals method

--- eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
+++ eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
@@ -1,55 +1,51 @@
 
 package ibm_project.stringpool;
 
-public class Application {
-	
-	 static int outerStaticValue = 100;
-	 
-	 class InnerClass {
-	        
-	        public void display() {
-	            System.out.println("Outer Static Value: " + outerStaticValue);
-	        }
-	    }
-	 
-	 static class StaticInnerClass {
-	        
-	        static int innerStaticValue = 200;
+class Employee {
+    private String name;
+    private int id;
 
-	        
-	        public static void displayStatic() {
-	            System.out.println("Static Inner Static Value: " + innerStaticValue);
-	        }
-
-	       
-	        public void display() {
-	            System.out.println("Accessing Static Inner Class Static Value: " + innerStaticValue);
-	        }
-	    }
-	 
-
-
-
-  
     
+    public Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
    
-    
+    @Override
+    public boolean equals(Object obj) {
+        
+        if (this == obj) {
+            return true;
+        }
+
+        
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+
+       
+        Employee otherEmployee = (Employee) obj;
+        return this.name.equals(otherEmployee.name) && this.id == otherEmployee.id;
+    }
+}
+
+
+public class Application {
+
 
     public static void main(String[] args) {
+    	 
+        Employee emp1 = new Employee("Alice", 101);
+        Employee emp2 = new Employee("Alice", 101);
+        Employee emp3 = new Employee("Bob", 102);
+
+        
+        System.out.println("emp1 equals emp2: " + emp1.equals(emp2)); 
+        System.out.println("emp1 equals emp3: " + emp1.equals(emp3)); 
     	
     	
-        Application outer = new Application();
-        Application.InnerClass inner = outer.new InnerClass();
-        inner.display();
-
-        System.out.println("\nDemonstrating Static Inner Class:");
-       
-        Application.StaticInnerClass.displayStatic();
-
-       
-        Application.StaticInnerClass staticInnerInstance = new Application.StaticInnerClass();
-        staticInnerInstance.display();
-
+        
     	 
 
         
