a3e2797370ca2e8d3d76167838dbba34b085cf3c
abhishek <abh9204@gmail.com>
2025-05-09 15:16:51 +0530

    special case of lambda function

--- eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
+++ eclipse-workspace/stringpool/src/main/java/ibm_project/stringpool/Application.java
@@ -1,37 +1,28 @@
 
 package ibm_project.stringpool;
 
-import java.util.ArrayList;
-import java.util.List;
+interface Printer{
+	
+	public void print(String name);
+	
+}
+class Process{
+	public void execute(String name , Printer printer) {
+		printer.print(name);
+	}
+}
+
 
 public class Application {
+	
+	public static void main(String[] args) {
+		Process process = new Process();
+		process.execute("HP printer Printing", System.out::println);
+		process.execute("canon", (name)->System.out.println(name+"printer printing..."));
+		
+	}
+	
 
-    public static void main(String[] args) {
-        // Create a list of strings
-        List<String> stringList = new ArrayList<>();
-        stringList.add("Apple");
-        stringList.add("Banana");
-        stringList.add("Cherry");
-
-        // Print the list
-        System.out.println("String List: " + stringList);
-
-        // Create a list of integers
-        List<Integer> integerList = new ArrayList<>();
-        integerList.add(1);
-        integerList.add(2);
-        integerList.add(3);
-
-        // Print the list
-        System.out.println("Integer List: " + integerList);
-
-        // Create a list of objects
-        List<Object> objectList = new ArrayList<>();
-        objectList.add("Apple");
-        objectList.add(1);
-        objectList.add(true);
-
-        // Print the list
-        System.out.println("Object List: " + objectList);
-    }
+   
+    
 }
