1998332f710340b201782acf9a95dd5d256cfa0e
abhishek <abh9204@gmail.com>
2025-05-13 10:52:15 +0530

    react action button

--- project-react-typescript/src/App.tsx
+++ project-react-typescript/src/App.tsx
@@ -1,10 +1,28 @@
 
 import './App.css'
 import ExpenseComponent from './components/ExpenseComponent'
+interface Grettingprop{
+  name: string;
+  city:string;
+
+}
 function Gretting({name,city}:Grettingprop){
   return(
     <>
       <h2>hello{name} from{city}</h2>
+    </>
+  )
+}
+
+interface Abuttonprops{
+  title:string;
+  disabled:boolean;
+
+}
+function Abutton({title,disabled}:Abuttonprops){
+  return(
+    <>
+      <button disabled={disabled}>{title}</button>
     </>
   )
 }
@@ -16,6 +34,7 @@ function App() {
        <div>
         <Gretting name="IBMER" city ="BNgalore"/>
         <ExpenseComponent/>
+        <Abutton title='Abutton' disabled = {false} />
        </div>
         
     </>
